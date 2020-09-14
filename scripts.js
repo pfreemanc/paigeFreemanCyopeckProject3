b260c1e9046005dc67b179b3df4b9dd8b525fe74
pfreemanc <pfreemanc@gmail.com>
2020-09-14 12:10:40 -0400

    smol js changes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-// Object pokemonTypes, with objects internally. Currently doing: fire, water, grass, electric, ground
-//type contains the
+// creating namespace object
+const pokemonApp = {};
 
-const pokemonTypes = {
+// Object pokemonTypes, with objects internally. All types are set up with all their mulitpliers.
+// source for multiplier information: https://bulbapedia.bulbagarden.net/wiki/Type#Type_chart
+pokemonApp.pokemonTypes = {
   fire: {
     fire: 0.5,
     water: 2,
@@ -10,6 +12,17 @@ const pokemonTypes = {
     ground: 2,
     flying: 1,
     ghost: 1,
+    normal: 1,
+    fighting: 1,
+    rock: 2,
+    poison: 1,
+    bug: 0.5,
+    steel: 0.5,
+    psychic: 1,
+    ice: 0.5,
+    dark: 1,
+    fairy: 0.5,
+    dragon: 1,
   },
   grass: {
     fire: 2,
@@ -19,6 +32,17 @@ const pokemonTypes = {
     ground: 0.5,
     flying: 2,
     ghost: 1,
+    normal: 1,
+    fighting: 1,
+    rock: 1,
+    poison: 2,
+    bug: 2,
+    steel: 1,
+    psychic: 1,
+    ice: 2,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
   },
   water: {
     fire: 0.5,
@@ -28,6 +52,17 @@ const pokemonTypes = {
     ground: 1,
     flying: 1,
     ghost: 1,
+    normal: 1,
+    fighting: 1,
+    rock: 1,
+    poison: 1,
+    bug: 1,
+    steel: 0.5,
+    psychic: 1,
+    ice: 0.5,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
   },
   electric: {
     fire: 1,
@@ -37,7 +72,17 @@ const pokemonTypes = {
     ground: 2,
     flying: 0.5,
     ghost: 1,
-
+    normal: 1,
+    fighting: 1,
+    rock: 1,
+    poison: 1,
+    bug: 1,
+    steel: 0.5,
+    psychic: 1,
+    ice: 1,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
   },
   ground: {
     fire: 1,
@@ -47,6 +92,17 @@ const pokemonTypes = {
     ground: 1,
     flying: 1,
     ghost: 1,
+    normal: 1,
+    fighting: 1,
+    rock: 0.5,
+    poison: 0.5,
+    bug: 1,
+    steel: 1,
+    psychic: 1,
+    ice: 2,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
   },
   flying: {
     fire: 1,
@@ -56,19 +112,262 @@ const pokemonTypes = {
     ground: 0,
     flying: 1,
     ghost: 1,
+    normal: 1,
+    fighting: 0.5,
+    rock: 2,
+    poison: 1,
+    bug: 0.5,
+    steel: 1,
+    psychic: 1,
+    ice: 2,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
   },
   ghost: {
     fire: 1,
     water: 1,
     grass: 1,
-    electric: 1, 
+    electric: 1,
     ground: 1,
     flying: 1,
     ghost: 2,
-  }
+    normal: 0,
+    fighting: 0,
+    rock: 1,
+    poison: 0.5,
+    bug: 0.5,
+    steel: 1,
+    psychic: 1,
+    ice: 1,
+    dark: 2,
+    fairy: 1,
+    dragon: 1,
+  },
+  normal: {
+    fire: 1,
+    water: 1,
+    grass: 1,
+    electric: 1,
+    ground: 1,
+    flying: 1,
+    ghost: 0,
+    normal: 1,
+    fighting: 2,
+    rock: 1,
+    poison: 1,
+    bug: 1,
+    steel: 1,
+    psychic: 1,
+    ice: 1,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
+  },
+  fighting: {
+    fire: 1,
+    water: 1,
+    grass: 1,
+    electric: 1,
+    ground: 1,
+    flying: 2,
+    ghost: 1,
+    normal: 1,
+    fighting: 1,
+    rock: 0.5,
+    poison: 1,
+    bug: 0.5,
+    steel: 1,
+    psychic: 2,
+    ice: 1,
+    dark: 0.5,
+    fairy: 2,
+    dragon: 1,
+  },
+  rock: {
+    fire: 0.5,
+    water: 2,
+    grass: 2,
+    electric: 1,
+    ground: 2,
+    flying: 0.5,
+    ghost: 1,
+    normal: 0.5,
+    fighting: 2,
+    rock: 1,
+    poison: 0.5,
+    bug: 1,
+    steel: 2,
+    psychic: 1,
+    ice: 1,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
+  },
+  poison: {
+    fire: 1,
+    water: 1,
+    grass: 0.5,
+    electric: 1,
+    ground: 2,
+    flying: 1,
+    ghost: 1,
+    normal: 1,
+    fighting: 0.5,
+    rock: 1,
+    poison: 0.5,
+    bug: 0.5,
+    steel: 1,
+    psychic: 2,
+    ice: 1,
+    dark: 1,
+    fairy: 0.5,
+    dragon: 1,
+  },
+  bug: {
+    fire: 2,
+    water: 1,
+    grass: 0.5,
+    electric: 1,
+    ground: 0.5,
+    flying: 2,
+    ghost: 1,
+    normal: 1,
+    fighting: 0.5,
+    rock: 2,
+    poison: 1,
+    bug: 1,
+    steel: 1,
+    psychic: 1,
+    ice: 1,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
+  },
+  steel: {
+    fire: 2,
+    water: 1,
+    grass: 0.5,
+    electric: 1,
+    ground: 2,
+    flying: 0.5,
+    ghost: 1,
+    normal: 0.5,
+    fighting: 2,
+    rock: 0.5,
+    poison: 0,
+    bug: 0.5,
+    steel: 0.5,
+    psychic: 0.5,
+    ice: 0.5,
+    dark: 1,
+    fairy: 0.5,
+    dragon: 0.5,
+  },
+  psychic: {
+    fire: 1,
+    water: 1,
+    grass: 1,
+    electric: 1,
+    ground: 1,
+    flying: 1,
+    ghost: 2,
+    normal: 1,
+    fighting: 0.5,
+    rock: 1,
+    poison: 1,
+    bug: 2,
+    steel: 1,
+    psychic: 0.5,
+    ice: 1,
+    dark: 2,
+    fairy: 1,
+    dragon: 1,
+  },
+  ice: {
+    fire: 2,
+    water: 1,
+    grass: 1,
+    electric: 1,
+    ground: 1,
+    flying: 1,
+    ghost: 1,
+    normal: 1,
+    fighting: 2,
+    rock: 2,
+    poison: 1,
+    bug: 1,
+    steel: 2,
+    psychic: 1,
+    ice: 0.5,
+    dark: 1,
+    fairy: 1,
+    dragon: 1,
+  },
+  dragon: {
+    fire: 0.5,
+    water: 0.5,
+    grass: 0.5,
+    electric: 0.5,
+    ground: 1,
+    flying: 1,
+    ghost: 1,
+    normal: 1,
+    fighting: 1,
+    rock: 1,
+    poison: 1,
+    bug: 1,
+    steel: 1,
+    psychic: 1,
+    ice: 2,
+    dark: 1,
+    fairy: 2,
+    dragon: 2,
+  },
+  dark: {
+    fire: 1,
+    water: 1,
+    grass: 1,
+    electric: 1,
+    ground: 1,
+    flying: 1,
+    ghost: 0.5,
+    normal: 1,
+    fighting: 2,
+    rock: 1,
+    poison: 1,
+    bug: 2,
+    steel: 1,
+    psychic: 0,
+    ice: 1,
+    dark: 0.5,
+    fairy: 2,
+    dragon: 1,
+  },
+  fairy: {
+    fire: 1,
+    water: 1,
+    grass: 1,
+    electric: 1,
+    ground: 1,
+    flying: 1,
+    ghost: 1,
+    normal: 1,
+    fighting: 0.5,
+    rock: 1,
+    poison: 2,
+    bug: 0.5,
+    steel: 2,
+    psychic: 1,
+    ice: 1,
+    dark: 0.5,
+    fairy: 1,
+    dragon: 1,
+  },
 };
 
-const pokemonTypeColors = {
+// creating the background color object.
+pokemonApp.pokemonTypeColors = {
   fire: "#F08030",
   grass: "#78C850",
   water: "#6890F0",
@@ -76,100 +375,140 @@ const pokemonTypeColors = {
   ground: "#E0C068",
   flying: "#A890F0",
   ghost: "#705898",
+  normal: "#A8A878",
+  fighting: "#C03028",
+  rock: "#B8A038",
+  poison: "#A040A0",
+  bug: "#A8B820",
+  steel: "#B8B8D0",
+  psychic: "#F85888",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  fairy: "#EE99AC",
 };
 
-// creating namespace object
-
-// Doc ready TODO remove this comment
-$(function(){
-  // adding all current types to the list in the ul
-  // TODO: refactor this. make better.
-  const displayCurrentTypes = function () {
-    for (let type in pokemonTypes) {
-      $(".listCurrentlySupported").append(`<li> ${type} </li>`);
-      // add buttons to li items in .typeSelector ul 
+pokemonApp.init = function () {
+  pokemonApp.displayCurrentTypes();
+  pokemonApp.generateTypes();
+  // event listener for when the user selects their type choice.
+  $(".type").on("click", function () {
+    pokemonApp.radioVal = $('#radio1:checked').val();
+    if(pokemonApp.radioVal === 'on') {
+      pokemonApp.radioVal = true;
+    } else {
+      pokemonApp.radioVal = false;
     }
-  };
-  
-  displayCurrentTypes();
-  
-  // refactor to call createButton
-  const generateTypes = function() {
-    for(let type in pokemonTypes) {
-      // creating the list item
-      const listItem = $('<li>');
-      // button with type grabbing the name
-      const button = $('<button>').addClass(type).addClass('type').text(type);
-      // chagning the button to the type colour
-      $(button).css('background-color', pokemonTypeColors[type]);
-      listItem.append(button);
-      $('.typeSelector').append(listItem);
-    }
-  }  
-  generateTypes();
+    // changes h2, removes h3 dynamically.
+    $("h2").text("You Chose....");
+    $("h3").empty();
+    $("form").empty();
+    // save user selection
+    // removing the type class in order to correct retrive the type information
+    $(this).removeClass("type");
+    const selectedClass = $(this).attr("class");
+    // emptying the list in order to populate information successfully.
+    $(".typeSelector").empty().addClass("listPostChoice");
+    pokemonApp.showInfo(selectedClass);
+  });
+};
 
-  // function for generating a button.
-  const createButton = function(type){
-    const button = $("<button>").addClass(type).addClass('type').text(type);
-    $(button).css("background-color", pokemonTypeColors[type]);
-    return button;
+// checks the radio value and calls the 
+
+// function that displays the current supported types that the user can select.
+pokemonApp.displayCurrentTypes = function () {
+  for (let type in pokemonApp.pokemonTypes) {
+    // add buttons to li items in .typeSelector ul
+    $(".listCurrentlySupported").append(`<li> ${type} </li>`);
   }
+};
 
-  // function that kicks off the display of the type information based on the user choice.
-  const showInfo = function(type) {
-    const userChoice = $('<li>').addClass("listUserChoice").html('<p>' + 'User Choice:');
-    userChoice.append(createButton(type));
-    $('.typeSelector').append(userChoice);
-    multiplierCalculator(type);
+// function that generates the types that are allowed to be chosen. Adds dynamically to the empty UL.
+pokemonApp.generateTypes = function () {
+  for (let type in pokemonApp.pokemonTypes) {
+    // creating the list item
+    const listItem = $("<li>");
+    // button with type grabbing the name
+    // TODO: refactor with generatebutton.
+    const button = pokemonApp.createButton(type);
+    // chagning the button to the type colour
+    listItem.append(button);
+    $(".typeSelector").append(listItem);
   }
+};
 
-  const multiplierCalculator = function(type) {
-    //create a loop that runs through the type mutlipliers for the selected user type
-    for(let multiplier in pokemonTypes[type]) {
-      const listItem = $('<li>').addClass("listTypeMultiplier");
-      //adding a div to hold the text results to style easier.
-      const container = $('<div>');
-      // generate the icon/button that shows the types. 
-      listItem.append(createButton(multiplier));
-      //generate the multiplier text information box.
-      const multiplierItem = $('<p>').text(`${pokemonTypes[type][multiplier]}x damage against ${type} type`);
-      // conditional statement to determine the effectiveness of the type, adding splash text based on that.
-      const splashText = $('<p>');
-      if(pokemonTypes[type][multiplier] === 0) {
-        $(splashText).text('No effect!');
-      } else if (pokemonTypes[type][multiplier] === 0.5) {
+// function that creates the button. Adds the class with the type that's passed on. Also changes the background colour to use the saved background colours that are in the pokemonTypeColors object
+pokemonApp.createButton = function (type) {
+  const button = $("<button>").addClass(type).addClass("type").text(type);
+  $(button).css("background-color", pokemonApp.pokemonTypeColors[type]);
+  return button;
+};
+
+// Function that wil call the multiplierCalculator. Also generates the user choice information.
+pokemonApp.showInfo = function (type) {
+  const userChoice = $("<li>").addClass("listUserChoice");
+  userChoice.append(pokemonApp.createButton(type));
+  $(".typeSelector").append(userChoice);
+  pokemonApp.multiplierCalculator(type, pokemonApp);
+};
+
+pokemonApp.multiplierCalculator = function (type, boolean) {
+  //create a loop that runs through the type mutlipliers for the selected user type
+  for (let multiplier in pokemonApp.pokemonTypes[type]) {
+    const listItem = $("<li>").addClass("listTypeMultiplier");
+    //adding a div to hold the text results to style easier.
+    const container = $("<div>");
+    // generate the icon/button that shows the types.
+    listItem.append(pokemonApp.createButton(multiplier));
+    //generate the multiplier text information box.
+    const multiplierItem = $("<p>").text(
+      `${pokemonApp.pokemonTypes[type][multiplier]}x damage against ${type} type`
+    );
+    // conditional statement to determine the effectiveness of the type, adding splash text based on that.
+    const splashText = $("<p>");
+
+    // condense, create another else if for elseif === 1 && has class toremove. only one conditional statement.
+    if(pokemonApp.radioVal) {
+      if (pokemonApp.pokemonTypes[type][multiplier] === 0) {
+        $(splashText).text("No effect!");
+      } else if (pokemonApp.pokemonTypes[type][multiplier] === 0.5) {
         $(splashText).text("It's not very effective!");
-      } else if (pokemonTypes[type][multiplier] === 2) {
+      } else if (pokemonApp.pokemonTypes[type][multiplier] === 2) {
         $(splashText).text("It's super effective!");
       } else {
         $(splashText).text("Regular damage!");
       }
-      // appending the container to the list item as well as adding the dynamically generated content to the container.
-      $(listItem).append($(container).append(multiplierItem).append(splashText));
-      //final step: adding to listItem.
-      $('.typeSelector').append(listItem);
+    } else {
+      if (pokemonApp.pokemonTypes[type][multiplier] === 0) {
+        $(splashText).text("No effect!");
+      } else if (pokemonApp.pokemonTypes[type][multiplier] === 0.5) {
+        $(splashText).text("It's not very effective!");
+      } else if (pokemonApp.pokemonTypes[type][multiplier] === 2) {
+        $(splashText).text("It's super effective!");
+      } else {
+        $(listItem).addClass("toRemove");
+      }
     }
+    $(listItem).append($(container).append(multiplierItem).append(splashText));
+    // appending the container to the list item as well as adding the dynamically generated content to the container.
+    //final step: adding to listItem.
+    $(".typeSelector").append(listItem);
+    $(".listTypeMultiplier.toRemove").hide();
   }
+};
 
-  // event listener for when the user selects their type choice.
-  $('.type').on("click", function() {
-    // save user selection
-    // removing the type class in order to correct retrive the type information
-    $(this).removeClass('type');
-    const selectedClass = $(this).attr('class');
-    // emptying the list in order to populate information successfully.
-    $('.typeSelector').empty().addClass('listPostChoice');
-    showInfo(selectedClass);
-  })
-  
-})
-
-//smooth scroll, foudn this code. 
-$(".btnGetStarted").click(function () {
-  $("html, body").animate(
-    {
-      scrollTop: $(".typeSelector").offset().top,
-    },
-    2000
-  );
+// Doc ready TODO remove this comment
+$(function () {
+  // adding all current types to the list in the ul
+  pokemonApp.init();
+  // function that kicks off the display of the type information based on the user choice.
+  //smooth scroll, foudn this code.
+  $(".btnGetStarted").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".typeSelector").offset().top,
+      },
+      2000
+    );
+  });
 });
